29d2513e307aff4141d68ceee0b5af2ac2600d6d
Mochammad Mustakim <mumustakim7@gmail.com>
2018-01-09 15:17:49 +0700

    Game ini dibuat untuk memenuhi tugas dari Workshop Produksi Game 3 ,dengan dosen pengampu bapak Zulhaydar Fairozal Akbar

---
+++ app/src/main/java/com/arbko/planeshooter/SharedPreferencesManager.java
@@ -0,0 +1,26 @@
+package com.arbko.planeshooter;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedPreferencesManager {
+
+    private String mName = "SpaceShooter";
+    private Context mContext;
+
+    public SharedPreferencesManager(Context context) {
+        mContext = context;
+    }
+
+    public void saveHighScore(int score){
+        SharedPreferences sp = mContext.getSharedPreferences(mName, Context.MODE_PRIVATE);
+        SharedPreferences.Editor e = sp.edit();
+        e.putInt("high_score", score);
+        e.commit();
+    }
+
+    public int getHighScore(){
+        SharedPreferences sp = mContext.getSharedPreferences(mName, Context.MODE_PRIVATE);
+        return sp.getInt("high_score", 0);
+    }
+}
